bb7097a6e43c39749bbf10d830f384a42ed93268
Alex Rupom Hasdak <alexrupom@hotmail.com>
2022-06-28 10:33:33 +0600

    Revert "rubocop checking"
    This reverts commit 35d2aab858ef969c20055172467e685ea89fa4c5.

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,7 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    def asslo
-      puts 'heloo' # sflsdjflsjdfljsdlfkjsdlfjldsjfkldsjfldksjflkdsjflkdsjflksdjlfkjsdlkfjsdlfjsdlkfjsdlfsdjf
-    end
   end
 end
