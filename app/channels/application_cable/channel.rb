35d2aab858ef969c20055172467e685ea89fa4c5
Alex Rupom Hasdak <alexrupom@hotmail.com>
2022-06-28 10:32:16 +0600

    rubocop checking

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,7 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    def asslo
+      puts 'heloo' # sflsdjflsjdfljsdlfkjsdlfjldsjfkldsjfldksjflkdsjflkdsjflksdjlfkjsdlkfjsdlfjsdlkfjsdlfsdjf
+    end
   end
 end
